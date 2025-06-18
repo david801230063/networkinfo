43ea228c5bbde4dcfde0b28518445b652551d591
david801230063 <davidpaul1595@gmail.com>
2025-06-18 07:13:20 +0530

    Initial Project Structure

---
+++ src/org/test/in/GitPush.java
@@ -0,0 +1,15 @@
+package org.test.in;
+
+public class GitPush {	
+		
+		public static void main(String[] args) {
+				
+				String name= ("Paul");
+				
+				System.out.println(name);
+				
+				
+					
+		}
+
+}
